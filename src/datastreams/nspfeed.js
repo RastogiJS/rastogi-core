b92c2458637d0ad9f20db130cf70d31a73e302a7
jhejderup <joseph.hejderup@gmail.com>
2016-10-12 17:43:55 +0200

    fix(nsp): poll is broken, we have to rely on a single get
    TAG: latest

--- src/datastreams/nspfeed.js
+++ src/datastreams/nspfeed.js
@@ -2,7 +2,7 @@ const Rx = require('rxjs/Rx')
 const nsp = require('./nsp')
 const npm = require('./npm')
 
-const nspFeed = nsp.pollWithdistinctUntilChanged
+const nspFeed = nsp.get.flatMap(x => Rx.Observable.from(x))
   .flatMap(adv => npm.fetchDocument(adv.module_name), (adv, doc) => {
     if (doc && doc.versions) {
       adv.module_versions = Object.keys(doc.versions)
